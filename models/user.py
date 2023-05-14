b5be11cfcdb3b4f3096b069e8cd2df4e2abe8bda
Reginald Amuzu <95586757+m00n-die@users.noreply.github.com>
2023-05-14 23:06:28 +0000

    add models/user.py

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""creates a User class"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """User class that inherits from BaseModel class"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
