2939865e223674dd9c2a916c950aa7d2561da88a
m00n-die <reginaldrock219@gmail.com>
2023-05-14 17:15:27 -0700

    final fixes

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module creates a City class"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """City class that inherits from BaseModel class"""
+    state_id = ""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module creates a State class"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """State class that inherits from BaseModel class"""
+    name = ""
